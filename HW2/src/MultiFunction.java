0686435ae5d0ea2f8c33917dee2feb3e9b2a8bde
dwseank <89531857+dwseank@users.noreply.github.com>
2023-06-06 18:29:10 +0300

    Update MultiFunction.java for new inheritance structure

--- HW2/src/MultiFunction.java
+++ HW2/src/MultiFunction.java
@@ -1,50 +1,63 @@
-/** A function that contains multiple functions, combined using an arithmetic operator. */
-public abstract class MultiFunction extends TwoFunction {
+/** A function that contains multiple (at least 2) functions, combined using an arithmetic operator. */
+public abstract class MultiFunction extends Function {
     protected final Function[] functions;
+    private final char operator;
 
     /**
      * Constructs a MultiFunction.
      *
      * @param operator the arithmetic operator used to combine the functions: +, -, *, /
-     * @param function1 the first function
-     * @param function2 the second function
-     * @param functions ...more functions
+     * @param firstFunction the first function
+     * @param secondFunction the second function
+     * @param moreFunctions more functions
      */
-    protected MultiFunction(char operator, Function function1, Function function2, Function... functions) {
-        super(operator, function1, function2);
-        this.functions = functions;
+    protected MultiFunction(char operator, Function firstFunction, Function secondFunction, Function... moreFunctions) {
+        this.operator = operator;
+        functions = new Function[moreFunctions.length + 2];
+        functions[0] = firstFunction;
+        functions[1] = secondFunction;
+        for (int i = 0; i < moreFunctions.length; i++) {
+            functions[i + 2] = moreFunctions[i];
+        }
     }
 
     @Override
     public final double valueAt(double x) {
-        double value = super.valueAt(x); // valueAt for 2 functions
-        for (Function function : functions) {
-            value = operateOn(value, function.valueAt(x)); // Add valueAt for extra functions
+        double value = functions[0].valueAt(x);
+        for (int i = 1; i < functions.length; i++) {
+            value = operateOn(value, functions[i].valueAt(x));
         }
         return value;
     }
 
     @Override
     public final String toString() {
-        String str = super.toString(); // toString for 2 functions
-        str = str.substring(1, str.length() - 1); // Remove brackets at ends
-        for (Function function : functions) {
-            str += " " + operator + " " + function; // Add more functions
+        String str = functions[0].toString();
+        for (int i = 1; i < functions.length; i++) {
+            str += " " + operator + " " + functions[i];
         }
         return "(" + str + ")";
     }
 
     /**
-     * Calculates the derivative for each extra function in the multi function.
-     * Extra functions are all functions after the first 2 functions.
+     * Performs an arithmetic operation on two operands.
      *
-     * @return derivative of each extra function
+     * @param a first operand
+     * @param b second operand
+     * @return result of operation
      */
-    protected final Function[] multiDerivative() {
-        Function[] derivativeFunctions = new Function[functions.length];
-        for (int i = 0; i < functions.length; i++) {
-            derivativeFunctions[i] = functions[i].derivative();
+    private double operateOn(double a, double b) {
+        switch (operator) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a / b;
+            default:
+                return a;
         }
-        return derivativeFunctions;
     }
 }
