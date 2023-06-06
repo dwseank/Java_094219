637468f8dec6569263b1d5e289e9c68015373e48
dwseank <89531857+dwseank@users.noreply.github.com>
2023-06-06 18:34:56 +0300

    Update Sum.java for new inheritance structure

--- HW2/src/Sum.java
+++ HW2/src/Sum.java
@@ -1,17 +1,17 @@
 /** A function that is the sum of 2 functions. */
-public class Sum extends TwoFunction {
+public class Sum extends MultiSum {
     /**
      * Constructs sum of 2 functions.
      *
-     * @param leftFunction the left function
-     * @param rightFunction the right function
+     * @param firstFunction the first function
+     * @param secondFunction the second function
      */
-    public Sum(Function leftFunction, Function rightFunction) {
-        super('+', leftFunction, rightFunction);
+    public Sum(Function firstFunction, Function secondFunction) {
+        super(firstFunction, secondFunction);
     }
 
     @Override
     public Sum derivative() {
-        return new Sum(function1.derivative(), function2.derivative());
+        return new Sum(functions[0].derivative(), functions[1].derivative());
     }
 }
