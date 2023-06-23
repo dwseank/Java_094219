1d2a8deb4ac310e1fa29ee68f4016a53d8ce18aa
dwseank <89531857+dwseank@users.noreply.github.com>
2023-06-23 03:20:28 +0300

    Part A

---
+++ HW4/src/LevelLargestSum.java
@@ -0,0 +1,50 @@
+import java.util.ArrayDeque;
+
+/** This class contains a single static function for finding the level with the largest sum in an integer binary tree. */
+public class LevelLargestSum {
+    /**
+     * Finds the level with the largest sum in an integer binary tree.
+     * If there are multiple levels with the largest sum, the higher level will be returned.
+     *
+     * @param root the root of the integer binary tree
+     * @return the level with the largest sum, or -1 if the tree is empty.
+     */
+    public static int getLevelWithLargestSum(BinNode<Integer> root) {
+        if (root == null) {
+            return -1;
+        }
+        int sum = 0;
+        int largestSum = sum;
+        int level = 0;
+        int largestSumLevel = level;
+
+        ArrayDeque<BinNode<Integer>> currentLevel = new ArrayDeque<>();
+        currentLevel.add(root);
+        ArrayDeque<BinNode<Integer>> nextLevel = new ArrayDeque<>();
+
+        while (!currentLevel.isEmpty()) {
+            for (BinNode<Integer> node : currentLevel) {
+                // Sum level
+                sum += node.getData();
+                // Get next level
+                if (node.getLeft() != null) {
+                    nextLevel.add(node.getLeft());
+                }
+                if (node.getRight() != null) {
+                    nextLevel.add(node.getRight());
+                }
+            }
+            // Update largest sum and level
+            if (sum > largestSum) {
+                largestSum = sum;
+                largestSumLevel = level;
+            }
+            // Next level
+            currentLevel = nextLevel;
+            nextLevel = new ArrayDeque<>();
+            sum = 0;
+            level++;
+        }
+        return largestSumLevel;
+    }
+}
---
+++ HW4/src/PathFromRoot.java
@@ -0,0 +1,23 @@
+/** This class contains a single static recursive function for validating the existence of a path in a character binary tree. */
+public class PathFromRoot {
+    /**
+     * Checks if a there is a path of characters in a binary tree that match the given string.
+     *
+     * @param root the root of the character binary tree
+     * @param str the string to find in the tree
+     * @return true if found path, false if path does not exist
+     */
+    public static boolean doesPathExist(BinNode<Character> root, String str) {
+        // Base case
+        if (str.isEmpty()) {
+            return true;
+        }
+        // Check if the current node contains the first character
+        if (root.getData().equals(str.charAt(0))) {
+            // Check if the left or the right subtree contains a valid path for the rest of the string
+            return doesPathExist(root.getLeft(), str.substring(1)) || doesPathExist(root.getRight(), str.substring(1));
+        }
+        // Path is not valid
+        return false;
+    }
+}
